648f5b60681b7a16d745adbfab79549985c79f9d
Addisalem Abera Wordofa <611123@cs.mum.edu>
2020-05-29 20:54:21 -0500

    add

---
+++ bankaccount/account.js
@@ -0,0 +1,16 @@
+
+function createAccount(){
+
+
+
+
+    
+}
+window.onload = function() {
+    var createBtn = document.getElementById("createAcct");
+    var txtacctName = document.getElementById("txtacctName");
+    var txtdeposit = document.getElementById("txtdeposit");
+    var txtarea = document.getElementById("txtarea");
+    createBtn.onclick = createAccount;
+
+};
