0e381b9184cc59a6438d781ca35e587143928cd8
Zelalem <zola145103@gmail.com>
2020-05-30 20:40:25 -0500

    updat

--- bankaccount/account.js
+++ bankaccount/account.js
@@ -1,52 +1,41 @@
-
 window.onload = function () {
   document.getElementById("createAcct").onclick = createAccount;
 }
-
-function NewAccount(name, dep) {
+function Account(name, dep) {
   var accname = name;
   var deposit = dep;
-
   var account = {
-
-    'getAccName': function () {
+    "getAccName": function () {
       return accname;
     },
-    'getDeposit': function () {
+    "getDeposit": function () {
       return deposit;
     },
-    'setAccName': function (name) {
+    "setAccName": function (name) {
       accname = name;
     },
-    'setDeposit': function (dep) {
+    "setDeposit": function (dep) {
       deposit = dep;
     }
   };
   return account;
 }
-
 var myList =  Array();
-
 function createAccount() {
   var accName = document.getElementById("accountname").value;
-  var dep = document.getElementById('txtdeposit').value;
-
-  var acc = NewAccount(accName, dep);
-  accountList(acc);
+  var deposit = document.getElementById('txtdeposit').value;
+  var account = Account(accName, deposit);
+  accountList(account);
 }
 //
 function accountList(account){
   myList.push(account);
-
-  var output = "";
+  var listacct = "";
   for (var j = 0; j < myList.length; j++) {
-    output += "Account Name : " + myList[j].getAccName()+ ", Balance : " + myList[j].getDeposit() + "\n";
+    listacct += "Account Name : " + myList[j].getAccName()+ "  Balance : " + myList[j].getDeposit() + "\n";
    }
-      document.getElementById("txtarea").value = output;
-
+      document.getElementById("txtarea").value = listacct;
   }
-
-
  
 
   
